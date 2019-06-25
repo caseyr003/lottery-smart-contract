05f3c2827a9ddc51094a36dac9c801e2cdf563f3
Casey Ross <caseyross003@gmail.com>
2019-06-25 15:24:48 -0500

    add additional tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -4,6 +4,8 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
+let lottery;
+let accounts;
 
 beforeEach(async () => {
     //list all accounts
@@ -11,7 +13,7 @@ beforeEach(async () => {
 
     //use account to deploy contract
     lottery = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [] })
+        .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: '1000000' });
 
 });
@@ -23,13 +25,56 @@ describe('Lottery', () => {
 
     it('has a manager', async () => {
         const manager = await lottery.methods.manager().call();
-        assert.equal(manager, accounts[0]);
+        assert.equal(accounts[0], manager);
     });
 
-    it('enters lottery', async () => {
-        await lottery.methods.enter().send({ from: accounts[1] });
-        const players = await lottery.methods.players().call();
-        assert.equal(players[0], accounts[1]);
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({ from: accounts[1], value: web3.utils.toWei('0.1', 'ether') });
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[1]
+        });
+        assert.equal(accounts[1], players[0]);
+        assert.equal(1, players.length)
+    });
+
+    it('allows multiple accounts to enter', async () => {
+        await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('0.1', 'ether') });
+        await lottery.methods.enter().send({ from: accounts[1], value: web3.utils.toWei('0.1', 'ether') });
+        await lottery.methods.enter().send({ from: accounts[2], value: web3.utils.toWei('0.1', 'ether') });
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[1]
+        });
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length)
+    });
+
+    it('requires ether to enter', async () => {
+        try {
+            await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('0.01', 'ether') });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('requires manager to pick winner', async () => {
+        try {
+            await lottery.methods.pickWinner().send({ from: accounts[1] });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('sends funds to winner', async () => {
+        await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei('1', 'ether') });
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+        assert(difference > web3.utils.toWei('0.9', 'ether'));
     });
 
 });
